4d9ed04355962e9ce60d54bbb2a5c13c1026d416
Joseph Adoga <adoga.joseph0@gmail.com>
2024-04-01 15:53:42 -0500

    Started JavaScript

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,6 +12,13 @@ function select(selector) {
 
 const timeDisplay = select('.time-display');
 const startButton = select('.start-button');
+const userInput = select('.word-input');
+const wordDisplay = select('.word-display');
+const userHits = select('.user-hits span');
+
+const copyWordBank = [...wordBank];
+let randomWord = '';
+let hits = 0;
 
 let timer = 99;
 function timeCount() {
@@ -25,6 +32,37 @@ function timeCount() {
     }, 1000);
 }
 
+function setRandomWord() {
+    const randomIndex = Math.floor(Math.random() * copyWordBank.length);
+    randomWord = copyWordBank[randomIndex];
+    copyWordBank.splice(randomIndex, 1);
+    wordDisplay.innerText = randomWord;
+    return randomWord;
+}
+
+function clearInput() {
+    userInput.value = '';
+}
+
+function checkInput() {
+    if (randomWord === userInput.value) {
+        hits++;
+        userHits.innerText = hits;
+        clearInput();
+        setRandomWord();
+    }
+}
+
+function resetButton() {
+    startButton.value = 'Restart';
+    startButton.style.backgroundColor = 'red';
+}
+
 listen('click', startButton, function() {
     timeCount();
+    setRandomWord();
+});
+
+listen('input', userInput, function() {
+    checkInput();
 });
