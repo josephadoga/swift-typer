589df7de74728fb33ad80997ebba78b73f7cc00a
Joseph Adoga <adoga.joseph0@gmail.com>
2024-04-01 14:37:12 -0500

    Started Timer

--- assets/script/app.js
+++ assets/script/app.js
@@ -9,3 +9,22 @@ function listen(event, selector, callback) {
 function select(selector) {
     return document.querySelector(selector);
 }
+
+const timeDisplay = select('.time-display');
+const startButton = select('.start-button');
+
+let timer = 99;
+function timeCount() {
+    setInterval(() => {
+        if (timer < 0) {
+            return;
+        } else {
+            timeDisplay.innerText = timer;
+            timer--;
+        }
+    }, 1000);
+}
+
+listen('click', startButton, function() {
+    timeCount();
+});
